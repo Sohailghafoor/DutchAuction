11a225bdfe1e4af622004351fbcb431f6306e301
SohailGhafoor <sohail7004@gmail.com>
2022-05-30 06:27:21 -0700

    Initial Commit

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const DutchAuctions = await hre.ethers.getContractFactory("DutchAuction");
+  const dutch = await DutchAuctions.deploy();
+
+  await dutch.deployed();
+
+  console.log("DutchAuction deployed to:", dutch.address);
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
